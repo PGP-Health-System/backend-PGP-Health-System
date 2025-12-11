74855ecd6d8bec6cec2a788c6d3e9697bca23efe
pedrooDevV <pedromplctitto@gmail.com>
2025-12-11 00:31:09 -0300

    [Pedro Costa] - Primeiras alterações, service de endereço criado, controller de endereço criado com sucesso!

--- src/main/java/br/com/HEALTHTRACK/API/HEALTHTRACK/DTO/UsuarioLoginDTO.java
+++ src/main/java/br/com/HEALTHTRACK/API/HEALTHTRACK/DTO/UsuarioLoginDTO.java
@@ -1,7 +1,11 @@
 package br.com.HEALTHTRACK.API.HEALTHTRACK.DTO;
 
 public record UsuarioLoginDTO(
+<<<<<<< HEAD
         String username,
+=======
+        String email,
+>>>>>>> ec9fed0 ([Pedro Costa] - Primeiras alterações, service de endereço criado, controller de endereço criado com sucesso!)
         String senha
 ) {
 }
