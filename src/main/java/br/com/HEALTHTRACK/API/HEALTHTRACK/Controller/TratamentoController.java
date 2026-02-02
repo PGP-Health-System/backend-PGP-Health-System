5064d85884ce0e4faf58dc8a986a19bc593128d9
pedrooDevV <pedromplctitto@gmail.com>
2026-02-01 21:55:46 -0300

    Feat: Adicionando Swagger em controllers

--- src/main/java/br/com/HEALTHTRACK/API/HEALTHTRACK/Controller/TratamentoController.java
+++ src/main/java/br/com/HEALTHTRACK/API/HEALTHTRACK/Controller/TratamentoController.java
@@ -5,7 +5,13 @@ import br.com.HEALTHTRACK.API.HEALTHTRACK.DTO.Tratamento.TratamentoDetalheDTO;
 import br.com.HEALTHTRACK.API.HEALTHTRACK.Exception.HandlerException.Tratamento.ErroCadastrarTratamento;
 import br.com.HEALTHTRACK.API.HEALTHTRACK.Security.SecurityConfiguration;
 import br.com.HEALTHTRACK.API.HEALTHTRACK.Service.TratamentoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +26,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/tratamento")
 @SecurityRequirement(name = SecurityConfiguration.SECURITY)
+@Tag(name = "Tratamentos", description = "Endpoints para gerenciamento de tratamentos médicos")
 public class TratamentoController {
 
     @Autowired
@@ -30,7 +37,22 @@ public class TratamentoController {
         return tratamentoService;
     }
 
-
+    @Operation(
+            summary = "Registrar tratamento",
+            description = "Registra um novo tratamento para o paciente"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Tratamento registrado com sucesso",
+                    content = @Content(schema = @Schema(implementation = TratamentoDetalheDTO.class))
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Erro ao cadastrar tratamento",
+                    content = @Content
+            )
+    })
     @PostMapping("/registra")
     public ResponseEntity<Map<String, TratamentoDetalheDTO>> registraTratamento(TratamentoDTO tratamentoDTO) {
         return ResponseEntity.status(200).body(Map.of("Sucesso!", tratamentoService.registra(tratamentoDTO)));
