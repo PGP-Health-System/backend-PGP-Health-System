871823aec2f98a4b36a0f0db2c1d46e438d14925
pedrooDevV <pedromplctitto@gmail.com>
2026-02-08 23:39:53 -0300

    Feat: novos metodos em MedicacaoPacienteService

--- src/main/java/br/com/HEALTHTRACK/API/HEALTHTRACK/Repository/MedicacaoPacienteRepository.java
+++ src/main/java/br/com/HEALTHTRACK/API/HEALTHTRACK/Repository/MedicacaoPacienteRepository.java
@@ -17,4 +17,15 @@ public interface MedicacaoPacienteRepository extends JpaRepository<MedicacaoPaci
     List<MedicacaoPaciente> findAllByMedicacao_codigoMedicamento(String s);
 
   Optional<MedicacaoPaciente> findByPacienteAndMedicacao(Paciente paciente, Medicacao medicacao);
+
+    @Query(
+            value = """
+            SELECT DISTINCT p.*
+            FROM paciente p
+            JOIN medicacao_paciente mp ON mp.paciente_id = p.id
+            WHERE mp.ativo = true
+        """,
+            nativeQuery = true
+    )
+  List<MedicacaoPaciente> findAtivos();
 }
